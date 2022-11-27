9fe337c0fb5ea5d56efdc621a331196588b469e6
MIKHAIL <MIKHAIL@DESKTOP-6MU8OHN>
2022-11-27 23:29:49 +0500

    Добавьте файлы проекта.

---
+++ Задание_11_Работа над ошибками/Linear.cs
@@ -0,0 +1,27 @@
+﻿namespace Задание_11_Работа_над_ошибками
+
+    internal struct Linear
+{
+    double k;
+    double b;
+
+    public Linear(double k, double b)
+    {
+        this.k = k;
+        this.b = b;
+    }
+
+    public string Root()
+    {
+        if (k == 0 && b == 0)
+        { return "x - любое"; }
+        else if (k == 0 && b != 0)
+        { return "Решений нет"; }
+        else
+        {
+            double x = (-b / k);
+            return $"x = {x}";
+        }
+    }
+}
+}
